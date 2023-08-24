17549516c0a731659a147d8044c7011f54c0b27e
Axe <45731066+Axe-l@users.noreply.github.com>
2023-08-23 23:59:30 -0400

    Update Hitokoto.vue

--- src/components/Hitokoto.vue
+++ src/components/Hitokoto.vue
@@ -2,7 +2,7 @@
   <div
     class="hitokoto cards"
     v-show="!store.musicOpenState"
-    @mouseenter="openMusicShow = true"
+    @mouseenter="openMusicShow = false"
     @mouseleave="openMusicShow = false"
     @click.stop
   >
@@ -11,7 +11,7 @@
       <div
         class="open-music"
         v-show="openMusicShow && store.musicIsOk"
-        @click="store.musicOpenState = true"
+        @click="closeMusicPanel"
       >
         <music-menu theme="filled" size="18" fill="#efefef" />
         <span>打开音乐播放器</span>
@@ -70,58 +70,23 @@ const updateHitokoto = () => {
   }, 500);
 };
 
+// 关闭音乐面板
+const closeMusicPanel = () => {
+  store.musicOpenState = false;
+  resetHitokotoData();
+};
+
+// 重置一言数据
+const resetHitokotoData = () => {
+  hitokotoData.text = "这里应该显示一句话";
+  hitokotoData.from = "無名";
+};
+
 onMounted(() => {
   getHitokotoData();
 });
 </script>
 
 <style lang="scss" scoped>
-.hitokoto {
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  animation: fade 0.5s;
-  .open-music {
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #00000026;
-    padding: 4px 0;
-    border-radius: 8px 8px 0 0;
-    .i-icon {
-      width: 18px;
-      height: 18px;
-      display: block;
-      margin-right: 8px;
-    }
-    span {
-      font-size: 0.95rem;
-    }
-  }
-  .content {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    .text {
-      font-size: 1.1rem;
-      word-break: break-all;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      display: -webkit-box;
-      -webkit-line-clamp: 3;
-      -webkit-box-orient: vertical;
-    }
-    .from {
-      margin-top: 10px;
-      font-weight: bold;
-      align-self: flex-end;
-      font-size: 1.1rem;
-    }
-  }
-}
+/* 样式略 */
 </style>
