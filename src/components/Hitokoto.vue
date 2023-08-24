fb10ea0bf10488b520ff898c718091d790c2cc03
Axe <45731066+Axe-l@users.noreply.github.com>
2023-08-24 00:18:42 -0400

    Update Hitokoto.vue

--- src/components/Hitokoto.vue
+++ src/components/Hitokoto.vue
@@ -2,7 +2,7 @@
   <div
     class="hitokoto cards"
     v-show="!store.musicOpenState"
-    @mouseenter="openMusicShow = false"
+    @mouseenter="openMusicShow = true"
     @mouseleave="openMusicShow = false"
     @click.stop
   >
@@ -11,7 +11,7 @@
       <div
         class="open-music"
         v-show="openMusicShow && store.musicIsOk"
-        @click="closeMusicPanel"
+        @click="store.musicOpenState = true"
       >
         <music-menu theme="filled" size="18" fill="#efefef" />
         <span>打开音乐播放器</span>
@@ -19,8 +19,8 @@
     </Transition>
     <!-- 一言内容 -->
     <div class="content" @click="updateHitokoto">
-      <span class="text">{{ hitokotoData.text }}</span>
-      <span class="from">-「&nbsp;{{ hitokotoData.from }}&nbsp;」</span>
+      <span class="text" v-show="store.musicOpenState">{{ hitokotoData.text }}</span>
+      <span class="from" v-show="store.musicOpenState">-「&nbsp;{{ hitokotoData.from }}&nbsp;」</span>
     </div>
   </div>
 </template>
@@ -38,8 +38,8 @@ const openMusicShow = ref(false);
 
 // 一言数据
 const hitokotoData = reactive({
-  text: "这里应该显示一句话",
-  from: "無名",
+  text: "empty",
+  from: "6UI",
 });
 
 // 获取一言数据
@@ -70,23 +70,59 @@ const updateHitokoto = () => {
   }, 500);
 };
 
-// 关闭音乐面板
-const closeMusicPanel = () => {
-  store.musicOpenState = false;
-  resetHitokotoData();
-};
-
-// 重置一言数据
-const resetHitokotoData = () => {
-  hitokotoData.text = "这里应该显示一句话";
-  hitokotoData.from = "無名";
-};
-
 onMounted(() => {
   getHitokotoData();
 });
 </script>
 
+
 <style lang="scss" scoped>
-/* 样式略 */
+.hitokoto {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  animation: fade 0.5s;
+  .open-music {
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #00000026;
+    padding: 4px 0;
+    border-radius: 8px 8px 0 0;
+    .i-icon {
+      width: 18px;
+      height: 18px;
+      display: block;
+      margin-right: 8px;
+    }
+    span {
+      font-size: 0.95rem;
+    }
+  }
+  .content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    .text {
+      font-size: 1.1rem;
+      word-break: break-all;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+    }
+    .from {
+      margin-top: 10px;
+      font-weight: bold;
+      align-self: flex-end;
+      font-size: 1.1rem;
+    }
+  }
+}
 </style>
