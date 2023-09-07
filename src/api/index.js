96db2a2b2e2f75b5b85f1f2bdb7497269fed0a12
Axe-l <45731066+Axe-l@users.noreply.github.com>
2023-09-06 22:15:43 -0400

    更快的加载

--- src/api/index.js
+++ src/api/index.js
@@ -41,16 +41,6 @@ export const getPlayerList = async (server, type, id) => {
 };
 
 /**
- * 一言
- */
-
-// 获取一言数据
-export const getHitokoto = async () => {
-  const res = await fetch("https://v1.hitokoto.cn");
-  return await res.json();
-};
-
-/**
  * 天气
  */
 
